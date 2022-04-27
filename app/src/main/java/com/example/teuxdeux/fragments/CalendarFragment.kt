44e61fed74a5b17c13d49a9f1515661cb8e98413
JDon25 <=>
2022-04-26 17:51:55 -0700

    working calendar
    Some calendar updates Still working on populating visible calendar and trying to make a recycle view for the day your on below calendar

--- app/src/main/java/com/example/teuxdeux/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/teuxdeux/fragments/CalendarFragment.kt
@@ -1,60 +1,126 @@
 package com.example.teuxdeux.fragments
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.service.controls.ControlsProviderService
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.teuxdeux.Model.ToDoListAdapter
+import com.example.teuxdeux.Model.ToDoTask
+import com.example.teuxdeux.NavigationActivity
 import com.example.teuxdeux.R
+import com.example.teuxdeux.activities.ManageTaskActivity
+import com.google.api.client.util.DateTime
+import com.google.api.services.calendar.model.Event
+import com.google.api.services.calendar.model.EventAttendee
+import com.google.api.services.calendar.model.EventDateTime
+import com.google.api.services.calendar.model.EventReminder
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CalendarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CalendarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+
+    val db = Firebase.firestore
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var documentArrayList : ArrayList<ToDoTask>
+    private lateinit var displayAdapter : ToDoListAdapter
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
-    }
+        val view: View = inflater.inflate(com.example.teuxdeux.R.layout.fragment_calendar , container , false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalendarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val calendarId = db.collection("username")
+
+        db.collection("taskData")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+
+                    val task = document.getString("task").toString()
+                    val deadline = document.getString("deadline").toString()
+                    val type = document.getString("type").toString()
+
+                    val event = Event()
+                        .setSummary(task)
+                        .setDescription(type)
+                    val startDateTime = DateTime(deadline)
+                    val start = EventDateTime()
+                        .setDateTime(startDateTime)
+                        .setTimeZone("America/Los_Angeles")
+                    calendarId.add(event)
                 }
             }
+            .addOnFailureListener { exception ->
+                Log.w(ControlsProviderService.TAG , "Error getting documents.", exception)
+            }
+
+
+        recyclerView = view.findViewById(com.example.teuxdeux.R.id.recycler)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.setHasFixedSize(true)
+        documentArrayList = arrayListOf()
+        displayAdapter = ToDoListAdapter(documentArrayList)
+        recyclerView.adapter = displayAdapter
+
+        getListFromFirebase()
+
+        displayAdapter.setOnItemClickListener(object : ToDoListAdapter.OnItemClickListener {
+            override fun onItemClick(itemView: View? , position: Int) {
+                val task = displayAdapter.taskList[position].task
+
+
+                val intent = Intent(activity , ManageTaskActivity::class.java)
+                intent.putExtra("task" , task)
+                startActivity(intent)
+            }
+
+            override fun onCheckBoxClicked(itemView: View? , position: Int , isChecked: Boolean) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        return view
+    }}
+
+private fun getListFromFirebase() {
+            val db = Firebase.firestore
+    if(db.collection("deadline") == Date()) {
+        db.collection("taskData").orderBy("deadline" , Query.Direction.ASCENDING)
+            .addSnapshotListener(object : EventListener<QuerySnapshot> {
+                override fun onEvent(
+                    value: QuerySnapshot? ,
+                    error: FirebaseFirestoreException?
+                ) {
+
+                    if (error != null) {
+                        Log.e("Firebase Error" , error.message.toString())
+                        return
+                    }
+                    }
+
+            })
     }
+        }
 }
