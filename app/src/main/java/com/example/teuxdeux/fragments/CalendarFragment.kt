d3cd4adfbc2d7fecb4a2ddd48b13d7d457759d94
JDon25 <=>
2022-04-28 17:16:09 -0700

    Calendar small update
    Changed deadline

--- app/src/main/java/com/example/teuxdeux/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/teuxdeux/fragments/CalendarFragment.kt
@@ -1,17 +1,18 @@
 package com.example.teuxdeux.fragments
 
+import android.app.Activity
+import android.app.usage.UsageEvents
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.service.controls.ControlsProviderService
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -34,24 +35,27 @@ import kotlinx.android.synthetic.main.fragment_calendar.*
 import java.util.*
 
 
+
 class CalendarFragment : Fragment() {
 
 
-
-    val db = Firebase.firestore
+    private val db = Firebase.firestore
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var documentArrayList : ArrayList<ToDoTask>
-    private lateinit var displayAdapter : ToDoListAdapter
+    private lateinit var documentArrayList: ArrayList<ToDoTask>
+    private lateinit var displayAdapter: ToDoListAdapter
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater , container: ViewGroup? ,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View = inflater.inflate(com.example.teuxdeux.R.layout.fragment_calendar , container , false)
+
+        val view: View =
+            inflater.inflate(com.example.teuxdeux.R.layout.fragment_calendar , container , false)
 
         val calendarId = db.collection("username")
+        val cal= cView
 
         db.collection("taskData")
             .get()
@@ -59,8 +63,14 @@ class CalendarFragment : Fragment() {
                 for (document in result) {
 
                     val task = document.getString("task").toString()
-                    val deadline = document.getString("deadline").toString()
+                    val deadline = document.getString("deadline")
                     val type = document.getString("type").toString()
+/*
+                    val intent = Intent(Intent.ACTION_INSERT)
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, deadline)
+                        .putExtra(CalendarContract.Events.TITLE, task)
+                        .putExtra(CalendarContract.Events.DESCRIPTION, type)*/
+
 
                     val event = Event()
                         .setSummary(task)
@@ -69,11 +79,16 @@ class CalendarFragment : Fragment() {
                     val start = EventDateTime()
                         .setDateTime(startDateTime)
                         .setTimeZone("America/Los_Angeles")
+
+                    event.start = start
+                    event.endTimeUnspecified
+
                     calendarId.add(event)
+
                 }
             }
             .addOnFailureListener { exception ->
-                Log.w(ControlsProviderService.TAG , "Error getting documents.", exception)
+                Log.w(ControlsProviderService.TAG , "Error getting documents." , exception)
             }
 
 
@@ -102,11 +117,12 @@ class CalendarFragment : Fragment() {
         })
 
         return view
-    }}
+    }
+}
 
 private fun getListFromFirebase() {
-            val db = Firebase.firestore
-    if(db.collection("deadline") == Date()) {
+    val db = Firebase.firestore
+    if (db.collection("deadline") == Date()) {
         db.collection("taskData").orderBy("deadline" , Query.Direction.ASCENDING)
             .addSnapshotListener(object : EventListener<QuerySnapshot> {
                 override fun onEvent(
@@ -118,9 +134,9 @@ private fun getListFromFirebase() {
                         Log.e("Firebase Error" , error.message.toString())
                         return
                     }
-                    }
+                }
 
             })
     }
-        }
 }
+
